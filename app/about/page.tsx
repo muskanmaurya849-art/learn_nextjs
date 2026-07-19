c86a90b260cc29b6e982a7ebff1c39d14f2d8430
Muskan Maurya <muskanmaurya@example.com>
2026-07-19 18:21:19 +0530

    change about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,5 @@
+import './globals.css'
 export default function Page(){
-    return <h1>This is my about page</h1>
+    return (
+    <h1 className="flex min-h-screen flex-col items-center justify-between p-24">This is my about page</h1>);
 }
